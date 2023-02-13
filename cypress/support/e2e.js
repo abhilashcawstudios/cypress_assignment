1f4cf8d7ebd76045cc069cf0eb16f54111c76310
abhilashcawstudios <abhilash.bommakanti@cawstudios.com>
2023-02-13 18:22:35 +0530

    .

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
