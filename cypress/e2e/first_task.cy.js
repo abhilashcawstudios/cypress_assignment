12521aca245d824aad88b28765d2546b4fe34110
abhilashcawstudios <abhilash.bommakanti@cawstudios.com>
2023-02-15 10:26:59 +0530

    .

--- cypress/e2e/first_task.cy.js
+++ cypress/e2e/first_task.cy.js
@@ -1,44 +1,67 @@
 /*
 Validate whether the search suggestion is not given to the user until 3 characters are populated.
 Validate results are displayed according to the search made by the user.
-Validate whether the user is able to apply the large size catalog filter for the T-shirt section.
-Validate whether the user is able to upload  a file on the contact us page.
-Add 5 products in the cart, validate total cart amount and individual product price both with and without discount.*/
+Add 5 products in the cart, validate total cart amount and individual product price
+Place the order for any one product selecting India as the country*/
 
 /// <reference types="cypress" />
-
 describe("First Assignment", () => {
-  })
-it("Task 1", () => {
-  cy.visit(Cypress.env('url2'));
-  cy.get("input[type=search]").click().type("Bro");
-  cy.get("h4[class=product-name]").should("be.visible");
-  cy.get("input[type=search]").click().clear().type("tomoto");
-  cy.get(".no-results").last().should("be.visible");
-})
+  beforeEach(() => {
+    cy.visit(Cypress.env("url2"));
+  });
 
-it("Task 2", () => {
-  cy.visit(Cypress.env('url2'));
-  cy.get("input[type=search]").click().type("ca");
-  cy.get('.product-name').eq(2).should("be.visible");
-  cy.get('.product-name').eq(3).should("be.visible");
-  cy.get('.product-name').eq(4).should("be.visible");
-  cy.get('.product-name').eq(5).should("be.visible");
-})
+  it("Task 1", () => {
+    cy.get("input[type=search]").click().type("Bro");
+    cy.get("h4[class=product-name]").should("be.visible");
+    cy.get("input[type=search]").click().clear().type("tomoto");
+    cy.get(".no-results").last().should("be.visible");
+    cy.get("input[type=search]").click().clear();
+  });
 
-it.only("Task 5", () => {
-    let total = 1;
-    cy.visit(Cypress.env('url2'));
-    [2, 3, 4, 5, 6].forEach((num) => {
-    cy.get(`button[type=button]="${num}"`)
-    })
-})
+  it("Task 2", () => {
+    cy.visit(Cypress.env("url2"));
+    cy.get("input[type=search]").click().type("ca");
+    cy.get(".product-name").eq(2).should("be.visible");
+    cy.get(".product-name").eq(3).should("be.visible");
+    cy.get(".product-name").eq(4).should("be.visible");
+    cy.get(".product-name").eq(5).should("be.visible");
+  });
 
-it("Task 5", () => {
-    let total = 0;
-    cy.visit("http://automationpractice.com/index.php");
-    [1, 2, 3, 4, 5].forEach((num) => {
-      cy.get(`[data-id-product="${num}"]`)
+  // it("Task 3", () => {
+  //   cy.visit(Cypress.env("url2"));
+  //   let price1;
+  //   let price2;
+  //   let price3;
+  //   let price4;
+  //   let price5;
+  // cy.get(".product-price")
+  //   .eq(0)
+  //   .then(function (price) {
+  //     this.price1 = price.text();
+  //   });
+  // cy.log(price1);
+  // cy.get("button[type=button]").eq(1).click();
+  // cy.get(".cart-icon").last().click();
+  // cy.get("div[class=action-block]").first().click();
+  // cy.get("tr td:nth-child(4) .amount").then((price) => {
+  //   expect(price.text()).equal(this.price1);
+  // });
+  // cy.get('button[type=button]').eq(2).click()
+  // cy.get('button[type=button]').eq(3).click()
+  // cy.get('button[type=button]').eq(4).click()
+  // cy.get('button[type=button]').eq(5).click()
+  // cy.get('.cart-icon').last().click()
+  // cy.get('div[class=action-block]').first().click()
 
-    })
-})
+  it("Task 4", () => {
+    cy.visit(Cypress.env("url2"));
+    cy.get("button[type=button]").eq(1).click();
+    cy.get(".cart-icon").last().click();
+    cy.get("div[class=action-block]").first().click();
+    cy.get("#root>div>div>div>div>button").click();
+    //cy.get('#root>div>div>div>div>div>select').click()
+    //cy.get('option:nth-child(89)')
+    cy.get("input[type=checkbox]").click();
+    cy.get("#root>div>div>div>div>button").click();
+  });
+});
